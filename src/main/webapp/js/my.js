87bdf25c1a38ac2fd0fa46e6e9586482591d8e63
Enzzo <enzzomatrix@gmail.com>
2024-05-08 16:47:17 +1100

    upd

--- src/main/webapp/js/my.js
+++ src/main/webapp/js/my.js
@@ -1,53 +1,48 @@
 $(document).ready(function() {
 $("h1").click(function (){
-    getPlayers();
-    getPlayersCount();
+    let selectorId = "selector";
+    let tableId = "mainTable";
+    makeSelector(selectorId);
+    loadPlayers(tableId);
 });
 })
 
 function getPlayersCount(){
-    $.ajax({
-        type: "GET",
-        url: "/rest/players/count",
-        dataType: "json",
-        success: function(data){
-            console.log(data);
+    $.get("rest/players/count", function(data){
+            let select = $("#rowsCount");
+            if(!select.length){
+                select = makeSelector(data);
+            }
         }
-    })
+    )
 }
 
 //  +-----------------------------------------------------------------------+
-//  |   getPlayers()                                                        |
+//  |   loadPlayers()                                                        |
 //  +-----------------------------------------------------------------------+
 //  |   gets data from /rest/players and fills #mainTable                   |
 //  +-----------------------------------------------------------------------+
-function getPlayers(){
-    $.ajax({
-        type: "GET",
-        url: "/rest/players",
-        dataType: "json",
-        success: function(data){
-            let mainTable = $("#mainTable");
-            if(!mainTable.length){
-                mainTable = makeBlankTable();
-            }
-            let body = mainTable.find("tbody");
-            if(!body.length){
-                body = mainTable.append("<tbody></tbody>").find("tbody").last();
-            }
-            data.forEach(function(player){
-                body.append("<tr></tr>")
-                    .find("tr").last()
-                    .append("<td>" + player.id + "</td>")
-                    .append("<td>" + player.name + "</td>")
-                    .append("<td>" + player.title + "</td>")
-                    .append("<td>" + player.race + "</td>")
-                    .append("<td>" + player.profession + "</td>")
-                    .append("<td>" + player.level + "</td>")
-                    .append("<td>" + player.birthday + "</td>")
-                    .append("<td>" + player.banned + "</td>")
-
-            })
+function loadPlayers(tableId = "tableId"){
+    let mainTable = $("#" + tableId);
+    if(!mainTable.length){
+        mainTable = makeBlankTable(tableId);
+    }
+    let body = mainTable.find("tbody");
+    if(!body.length){
+        body = mainTable.append("<tbody></tbody>").find("tbody").last();
+    }
+    $.get("/rest/players", function(data){
+        for(let i = 0; i < data.length; i++){
+            body.append("<tr></tr>")
+                .find("tr").last()
+                .append("<td>" + data[i].id + "</td>")
+                .append("<td>" + data[i].name + "</td>")
+                .append("<td>" + data[i].title + "</td>")
+                .append("<td>" + data[i].race + "</td>")
+                .append("<td>" + data[i].profession + "</td>")
+                .append("<td>" + data[i].level + "</td>")
+                .append("<td>" + new Date(data[i].birthday).toLocaleDateString() + "</td>")
+                .append("<td>" + data[i].banned + "</td>")
         }
     })
 }
@@ -56,10 +51,10 @@ function getPlayers(){
 //  +-----------------------------------------------------------------------+
 //  |   makes blank table with header                                       |
 //  +-----------------------------------------------------------------------+
-function makeBlankTable(){
+function makeBlankTable(tableId = "tableId"){
     let table = $(document.body).append("<table></table>")
         .find("table").last()
-        .attr("id", "mainTable");
+        .attr("id", tableId);
 
         table.append("<thead></thead>").find("thead").last()
             .append("<tr></tr>").find("tr").last()
@@ -73,4 +68,15 @@ function makeBlankTable(){
                 .append("<th>Banned</th>")
 
     return table;
+}
+
+function makeSelector(selectorId = "selectorId"){
+    let select = $("#" + selectorId);
+    if(!select.length){
+        select = $(document.body).append("<select></select>").find("select").last().attr("id", selectorId);
+        select.append("<option>" + 3 + "</option>");
+        select.append("<option>" + 5 + "</option>");
+        select.append("<option>" + 10 + "</option>");
+        select.append("<option>" + 20 + "</option>");
+    }
 }

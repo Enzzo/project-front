3f0efa29a158882616606209c1463c76bff0d30d
Enzzo <enzzomatrix@gmail.com>
2024-05-16 11:47:33 +1100

    upd

--- src/main/webapp/js/my.js
+++ src/main/webapp/js/my.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-    makeNavPages(getPagesCount());
-    loadPlayers(getRowsCount(), 0);
 
+    showPage(0);
     let rows = $("#rowsCount");
     rows.change(function () {
-        makeNavPages(getPagesCount());
-        loadPlayers(getRowsCount(), 0);
+        showPage(getActivePage());
     });
 })
 
@@ -23,6 +21,7 @@ function loadPlayers(rows, page){
     $.get(url, function(data){
         makeTable(data, tbody);
     });
+    changeNumberStyle(page);
 }
 
 //  +-----------------------------------------------------------------------+
@@ -42,26 +41,23 @@ function makeTable(data, table){
             .append("<td>" + data[i].level + "</td>")
             .append("<td>" + new Date(data[i].birthday).toLocaleDateString() + "</td>")
             .append("<td>" + data[i].banned + "</td>")
+            .append("<td><img src = \"img\\edit.png\"></td>")
+            .append("<td><img src = \"img\\delete.png\"></td>").on("click", function() {
+                deleteRequest(data[i].id);
+        });
     }
 }
 
 function makeNavPages(pages){
     let list = $("#pagination");
     list.empty();
-    list.append("<li><<</li>");
-    let prev = null;
+    // list.css("cursor", "pointer");
     for(let i = 0; i < pages; ++i){
         let li = list.append("<li>"+(i+1)+"</li>").find("li").last();
         li.on("click", function(){
-            if(prev != null) {
-                prev.css("color", "black");
-            }
             loadPlayers(getRowsCount(), $(this).text()-1);
-            $(this).css("color", "red");
-            prev = $(this);
         });
     }
-    list.append("<li>>></li>");
 }
 
 function getRowsCount(){
@@ -80,10 +76,34 @@ function getPlayersCount(){
     return count;
 }
 
-function getRowsCount(){
-    return $("#rowsCount").val();
-}
-
 function getPagesCount(){
     return Math.ceil(getPlayersCount() / getRowsCount());
+}
+
+function showPage(page){
+    makeNavPages(getPagesCount());
+    loadPlayers(getRowsCount(), page);
+}
+
+function changeNumberStyle(page) {
+    let list = $("#pagination");
+    list.find("li").css("color", "black").removeAttr("id");
+    list.find("li").eq(page).css("color", "red").attr("id", "active");
+}
+
+function deleteRequest(id){
+    $.ajax({
+        url: "/rest/players/" + id,
+        async: false,
+        type: "DELETE",
+        success: function(){
+            showPage(getActivePage());
+        }
+    })
+}
+
+function getActivePage(){
+    let page = 0;
+    page = $("#pagination").find("#active").text()-1;
+    return page;
 }
